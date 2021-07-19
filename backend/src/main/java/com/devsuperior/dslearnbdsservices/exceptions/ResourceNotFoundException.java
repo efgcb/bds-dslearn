c243820c549defb782eec303e593ee013a0fdb40
eduardo.fgoncalves <efgcb@windowslive.com>
2021-07-19 14:25:47 -0300

    Exceptions, validation, security

---
+++ backend/src/main/java/com/devsuperior/dslearnbdsservices/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dslearnbdsservices.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
